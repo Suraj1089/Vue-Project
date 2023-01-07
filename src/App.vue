a91693b09971649dca51fb3c82622866a5988dc1
Suraj Pisal <surajpisal113@gmail.com>
2023-01-07 15:12:24 +0530

    added navbar and footer with login page isssue=footer margin in not set to coreect bottom

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <NavbarComp v-bind:links="['Home', 'About', 'Contact']"/>
+  <LoginComp />
+  <FooterComp />
+</template>
+
+<script>
+import NavbarComp from './components/Navbar'
+import LoginComp from './components/Login'
+import FooterComp from './components/Footer'
+export default {
+  name: 'App',
+  components: {
+    NavbarComp,
+    LoginComp,
+    FooterComp
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="w-100 bg-pink-800 text-white" style="margin-top: 20px;">
+      <h4 class="text-center py-5">suraj@ {{ currentYear }} </h4>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterComp',
+    data() {
+      return {
+        currentYear:new Date().getFullYear(),
+      }
+    },
+    methods: {
+      
+    }
+}
+</script>
+
+<style>
+</style>
